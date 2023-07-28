ee3b97b6d4c400ced466cc064de7244468d68d88
Pablo Rizo <prozasoconw4@gmail.com>
2023-07-28 16:15:49 -0600

    FEAT: PARCHE PARA HACER EL LAS RESPUESTAS FLATTEN PARA TRANSPOLACION DE SOCKETS

--- server/middlewares/io.js
+++ server/middlewares/io.js
@@ -3,6 +3,47 @@
 const { getService } = require('../utils/getService');
 const { isValidContentTypeRoute } = require('../utils/isValidContentTypeRoute');
 
+
+const strapiFlatten = (data) => {
+	const isObject = (data) =>
+	  Object.prototype.toString.call(data) === "[object Object]";
+	const isArray = (data) =>
+	  Object.prototype.toString.call(data) === "[object Array]";
+  
+	const flatten = (data) => {
+	  if (!data.attributes) return data;
+  
+	  return {
+		id: data.id,
+		...data.attributes,
+	  };
+	};
+  
+	if (isArray(data)) {
+	  return data.map((item) => strapiFlatten(item));
+	}
+  
+	if (isObject(data)) {
+	  if (isArray(data.data)) {
+		data = [...data.data];
+	  } else if (isObject(data.data)) {
+		data = flatten({ ...data.data });
+	  } else if (data.data === null) {
+		data = null;
+	  } else {
+		data = flatten(data);
+	  }
+  
+	  for (const key in data) {
+		data[key] = strapiFlatten(data[key]);
+	  }
+  
+	  return data;
+	}
+  
+	return data;
+  };
+
 const io = async (strapi, ctx, next) => {
 	const settingsService = getService({ name: 'settingsService' });
 	const { contentTypes } = await settingsService.get();
@@ -48,7 +89,9 @@ const io = async (strapi, ctx, next) => {
 		return;
 	}
 
-	strapi.$io.emit(model, ctx.body);
+	console.log(`Emitting ${model} event`);
+
+	strapi.$io.emit(model, strapiFlatten(ctx.body));
 };
 
 module.exports = ({ strapi }) => {
